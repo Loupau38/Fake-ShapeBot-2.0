8c1eb853e0297b1d8b01e159746d739e7d3261ae
Loupau 38 <pilou38207bis@gmail.com>
2023-07-31 20:04:59 +0200

    Overhaul shape border

--- shapeViewer.py
+++ shapeViewer.py
@@ -3,7 +3,7 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = ""
 del os
 import pygame
 import globalInfos
-LAYER_SIZE_REDUCTION = 0.70 # maybe it should be 0.76, maybe not
+LAYER_SIZE_REDUCTION = 0.7 # maybe it should be 0.76, maybe not
 COLORS = {
     "u":(187,187,186),
     "r":(255,0,0),
@@ -19,92 +19,123 @@ SHAPE_BORDER_COLOR = (0,0,0)
 BG_CIRCLE_COLOR = (31,41,61,25)
 DEFAULT_IMAGE_SIZE = 602
 DEFAULT_BG_CIRCLE_DIAMETER = 520
-DEFAULT_SHAPE_DIAMETER = 422
-DEFAULT_BORDER_SIZE = 25 # should be 15, artificially increased to look better
+DEFAULT_SHAPE_DIAMETER = 407
+DEFAULT_BORDER_SIZE = 15
+FAKE_SURFACE_SIZE = globalInfos.INITIAL_SHAPE_SIZE
+SIZE_CHANGE_RATIO = FAKE_SURFACE_SIZE / DEFAULT_IMAGE_SIZE
+SHAPE_SIZE = DEFAULT_SHAPE_DIAMETER * SIZE_CHANGE_RATIO
+SHAPE_BORDER_SIZE = round(DEFAULT_BORDER_SIZE*SIZE_CHANGE_RATIO)
+BG_CIRCLE_DIAMETER = DEFAULT_BG_CIRCLE_DIAMETER * SIZE_CHANGE_RATIO
+SHAPE_SIZE_ON_2 = SHAPE_SIZE/2
+SHAPES_SHAPE = {
+    "C" : {
+        "type" : "circle",
+        "pos" : (0,SHAPE_SIZE_ON_2,SHAPE_SIZE_ON_2)
+    },
+    "R" : {
+        "type" : "rect",
+        "pos" : (0,0,SHAPE_SIZE_ON_2,SHAPE_SIZE_ON_2)
+    },
+    "S" : {
+        "type" : "polygon",
+        "points" : [(SHAPE_SIZE_ON_2,0),(SHAPE_SIZE/4,SHAPE_SIZE_ON_2),(0,SHAPE_SIZE_ON_2),(0,SHAPE_SIZE/4)]
+    },
+    "W" : {
+        "type" : "polygon",
+        "points" : [(SHAPE_SIZE_ON_2,0),(SHAPE_SIZE_ON_2,SHAPE_SIZE_ON_2),(0,SHAPE_SIZE_ON_2),(0,SHAPE_SIZE/4)]
+    },
+    "P" : {
+        "type" : "circle",
+        "pos" : (SHAPE_SIZE/6,SHAPE_SIZE/3,SHAPE_SIZE/12),
+        "color" : (122,148,166),
+        "border" : False
+    },
+    "c" : {
+        "type" : "circle",
+        "pos" : (0,SHAPE_SIZE_ON_2,SHAPE_SIZE_ON_2),
+        "color" : (50,160,180),
+        "border" : False
+    }
+}
 def preRenderQuadrants() -> None:
     global preRenderedQuadrants
-    preRenderedQuadrants = {}
-    fakeSurfaceSize = globalInfos.INITIAL_SHAPE_SIZE
-    shapeSize = (fakeSurfaceSize*DEFAULT_SHAPE_DIAMETER)/DEFAULT_IMAGE_SIZE
-    shapeBorderSize = round((fakeSurfaceSize*DEFAULT_BORDER_SIZE)/DEFAULT_IMAGE_SIZE)
-    shapeSizeOn2 = shapeSize/2
-    shapesShape = {
-        "C" : {
-            "type" : "circle",
-            "pos" : (0,shapeSizeOn2,shapeSizeOn2)
-        },
-        "R" : {
-            "type" : "rect",
-            "pos" : (0,0,shapeSizeOn2,shapeSizeOn2)
-        },
-        "S" : {
-            "type" : "polygon",
-            "points" : [(shapeSizeOn2,0),(shapeSize/4,shapeSizeOn2),(0,shapeSizeOn2),(0,shapeSize/4)]
-        },
-        "W" : {
-            "type" : "polygon",
-            "points" : [(shapeSizeOn2,0),(shapeSizeOn2,shapeSizeOn2),(0,shapeSizeOn2),(0,shapeSize/4)]
-        },
-        "P" : {
-            "type" : "circle",
-            "pos" : (shapeSize/6,shapeSize/3,shapeSize/12),
-            "color" : (122,148,166),
-            "border" : False
-        },
-        "c" : {
-            "type" : "circle",
-            "pos" : (0,shapeSizeOn2,shapeSizeOn2),
-            "color" : (50,160,180),
-            "border" : False
-        }
-    }
-    for shapeKey,shape in shapesShape.items():
+    for shapeKey,shape in SHAPES_SHAPE.items():
         if shape.get("color") is None:
             colors = COLORS
         else:
             colors = {"-":shape["color"]}
         for colorKey, colorValue in colors.items():
-            quadSurface = pygame.Surface((shapeSizeOn2,shapeSizeOn2),flags=pygame.SRCALPHA)
+            quadSurface = pygame.Surface((SHAPE_SIZE_ON_2,SHAPE_SIZE_ON_2),flags=pygame.SRCALPHA)
             if shape["type"] == "circle":
                 pygame.draw.circle(quadSurface,colorValue,
                     (shape["pos"][0],shape["pos"][1]),shape["pos"][2])
-                if shape.get("border") is not False:
-                    pygame.draw.circle(quadSurface,SHAPE_BORDER_COLOR,
-                        (shape["pos"][0],shape["pos"][1]),shape["pos"][2],shapeBorderSize)
-                    pygame.draw.line(quadSurface,SHAPE_BORDER_COLOR,(0,0),(0,shapeSizeOn2),shapeBorderSize)
-                    pygame.draw.line(quadSurface,SHAPE_BORDER_COLOR,(0,shapeSizeOn2),(shapeSizeOn2,shapeSizeOn2),shapeBorderSize)
             elif shape["type"] == "rect":
-                quadSurface.fill(SHAPE_BORDER_COLOR)
-                pygame.draw.rect(quadSurface,colorValue,pygame.Rect(
-                    shapeBorderSize/2,
-                    shapeBorderSize,
-                    shapeSizeOn2-(1.5*shapeBorderSize),
-                    shapeSizeOn2-(1.5*shapeBorderSize)))
+                pygame.draw.rect(quadSurface,colorValue,
+                    pygame.Rect(
+                        shape["pos"][0],
+                        shape["pos"][1],
+                        shape["pos"][2],
+                        shape["pos"][3]))
             else:
                 pygame.draw.polygon(quadSurface,colorValue,shape["points"])
-                pygame.draw.polygon(quadSurface,SHAPE_BORDER_COLOR,shape["points"],shapeBorderSize)
             preRenderedQuadrants[shapeKey+colorKey] = quadSurface
+preRenderedQuadrants:dict[str,pygame.Surface] = {}
 def renderShape(shapeCode:str,surfaceSize:int) -> pygame.Surface:
-    fakeSurfaceSize = globalInfos.INITIAL_SHAPE_SIZE
-    bgCircleDiameter = (fakeSurfaceSize*DEFAULT_BG_CIRCLE_DIAMETER)/DEFAULT_IMAGE_SIZE
-    shapeSize = (fakeSurfaceSize*DEFAULT_SHAPE_DIAMETER)/DEFAULT_IMAGE_SIZE
     decomposedShapeCode = shapeCode.split(":")
     numQuads = int(len(decomposedShapeCode[0])/2)
     decomposedShapeCode = [[layer[i*2:(i*2)+2] for i in range(numQuads)] for layer in decomposedShapeCode]
-    returnSurface = pygame.Surface((fakeSurfaceSize,fakeSurfaceSize),pygame.SRCALPHA)
-    pygame.draw.circle(returnSurface,BG_CIRCLE_COLOR,(fakeSurfaceSize/2,fakeSurfaceSize/2),bgCircleDiameter/2)
+    returnSurface = pygame.Surface((FAKE_SURFACE_SIZE,FAKE_SURFACE_SIZE),pygame.SRCALPHA)
+    pygame.draw.circle(returnSurface,BG_CIRCLE_COLOR,(FAKE_SURFACE_SIZE/2,FAKE_SURFACE_SIZE/2),BG_CIRCLE_DIAMETER/2)
     for layerIndex, layer in enumerate(decomposedShapeCode):
+        curLayerSizeReduction = LAYER_SIZE_REDUCTION ** layerIndex
         for quadIndex, quad in enumerate(layer):
             if quad.startswith("-"):
                 continue
-            tempLayerSurface = pygame.Surface((shapeSize,shapeSize),flags=pygame.SRCALPHA)
+            shapeShape = SHAPES_SHAPE[quad[0]]
             quadSurface = preRenderedQuadrants[quad]
-            tempLayerSurface.blit(quadSurface,(tempLayerSurface.get_width()/2,0))
+            resizedQuadSurfaceSize = round(SHAPE_SIZE_ON_2*curLayerSizeReduction)
+            withBorderQuadSurface = pygame.Surface(
+                (resizedQuadSurfaceSize+SHAPE_BORDER_SIZE,resizedQuadSurfaceSize+SHAPE_BORDER_SIZE),
+                pygame.SRCALPHA)
+            quadSurface = pygame.transform.scale(quadSurface,(resizedQuadSurfaceSize,resizedQuadSurfaceSize))
+            withBorderQuadSurface.blit(quadSurface,(SHAPE_BORDER_SIZE/2,SHAPE_BORDER_SIZE/2))
+            if shapeShape.get("border") is not False:
+                if shapeShape["type"] == "circle":
+                    pygame.draw.circle(withBorderQuadSurface,SHAPE_BORDER_COLOR,
+                        (
+                            (shapeShape["pos"][0]*curLayerSizeReduction)+(SHAPE_BORDER_SIZE/2),
+                            (shapeShape["pos"][1]*curLayerSizeReduction)+(SHAPE_BORDER_SIZE/2)),
+                        (shapeShape["pos"][2]*curLayerSizeReduction)+(SHAPE_BORDER_SIZE/2),
+                        SHAPE_BORDER_SIZE,
+                        draw_top_right=True)
+                    pygame.draw.line(withBorderQuadSurface,SHAPE_BORDER_COLOR,
+                        (SHAPE_BORDER_SIZE/2,0),
+                        (SHAPE_BORDER_SIZE/2,resizedQuadSurfaceSize+SHAPE_BORDER_SIZE),
+                        SHAPE_BORDER_SIZE)
+                    pygame.draw.line(withBorderQuadSurface,SHAPE_BORDER_COLOR,
+                        (0,resizedQuadSurfaceSize+(SHAPE_BORDER_SIZE/2)),
+                        (resizedQuadSurfaceSize+SHAPE_BORDER_SIZE,resizedQuadSurfaceSize+(SHAPE_BORDER_SIZE/2)),
+                        SHAPE_BORDER_SIZE)
+                elif shapeShape["type"] == "rect":
+                    pygame.draw.rect(withBorderQuadSurface,SHAPE_BORDER_COLOR,
+                        pygame.Rect(
+                            shapeShape["pos"][0]*curLayerSizeReduction,
+                            shapeShape["pos"][1]*curLayerSizeReduction,
+                            (shapeShape["pos"][2]*curLayerSizeReduction)+SHAPE_BORDER_SIZE,
+                            (shapeShape["pos"][3]*curLayerSizeReduction)+SHAPE_BORDER_SIZE
+                        ),
+                        SHAPE_BORDER_SIZE)
+                else:
+                    scaledPoints = [
+                        ((point[0]*curLayerSizeReduction)+(SHAPE_BORDER_SIZE/2),
+                         (point[1]*curLayerSizeReduction)+(SHAPE_BORDER_SIZE/2)) for point in shapeShape["points"]]
+                    pygame.draw.polygon(withBorderQuadSurface,SHAPE_BORDER_COLOR,scaledPoints,SHAPE_BORDER_SIZE)
+                    for point in scaledPoints:
+                        pygame.draw.circle(withBorderQuadSurface,SHAPE_BORDER_COLOR,point,(SHAPE_BORDER_SIZE/2)-1)
+            tempLayerSurface = pygame.Surface(((resizedQuadSurfaceSize*2)+SHAPE_BORDER_SIZE,(resizedQuadSurfaceSize*2)+SHAPE_BORDER_SIZE),flags=pygame.SRCALPHA)
+            tempLayerSurface.blit(withBorderQuadSurface,(resizedQuadSurfaceSize,0))
             tempLayerSurface = pygame.transform.rotate(tempLayerSurface,-((360/numQuads)*quadIndex))
-            tempLayerSurface = pygame.transform.scale(tempLayerSurface,(
-                tempLayerSurface.get_width()*(LAYER_SIZE_REDUCTION**layerIndex),
-                tempLayerSurface.get_height()*(LAYER_SIZE_REDUCTION**layerIndex)))
             returnSurface.blit(tempLayerSurface,(
-            (fakeSurfaceSize/2)-(tempLayerSurface.get_width()/2),
-            (fakeSurfaceSize/2)-(tempLayerSurface.get_height()/2)))
+            (FAKE_SURFACE_SIZE/2)-(tempLayerSurface.get_width()/2),
+            (FAKE_SURFACE_SIZE/2)-(tempLayerSurface.get_height()/2)))
     return pygame.transform.smoothscale(returnSurface,(surfaceSize,surfaceSize)) # pygame doesn't work well at low resolution so render at size 500 then downscale to the desired size
